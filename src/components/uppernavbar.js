2b3f731e04d909bd136efa839680d186da67920e
farhan0184 <shahariar0184@gmail.com>
2023-08-09 16:13:08 +0600

    responsive error solve

--- src/components/uppernavbar.js
+++ src/components/uppernavbar.js
@@ -9,7 +9,7 @@ const UpperNavbar = ({setShowModal}) => {
                     <div key={item.id} className={`flex gap-1 items-center px-3 cursor-pointer hover:text-yellow-300 ${item.id === 1 || item.id === 10? "border-none": "border-r-[1px]"}`}>
                         {item.img}
                         <div className="md:text-[7px] lg:text-[10px] xl:text-[14px] 2xl:text-[18px]">
-                        {item.id === 7 || item.id === 8 ? <button onClick={()=>setShowModal(true)}>{item.title}</button> : <button>{item.title}</button>}
+                        {item.id === 7 || item.id === 8 ? <button >{item.title}</button> : <button>{item.title}</button>}
                         </div>
                     </div>
                 ))
@@ -19,3 +19,6 @@ const UpperNavbar = ({setShowModal}) => {
 };
 
 export default UpperNavbar;
+
+
+// onClick={()=>setShowModal(true)}
